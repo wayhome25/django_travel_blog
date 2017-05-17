ce982456ad639d77ac6a49d7906ca2adb89b128b
wayhome25 <siwabada@gmail.com>
2017-05-17 18:18:13 +0900

    회원가입 커스텀 - 이메일 입력받기

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,15 @@
 from django.conf import settings
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
 
 def signup(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = SignupForm(request.POST)
 		if form.is_valid():
 			user = form.save()
 			return redirect(settings.LOGIN_URL)
 	else:
-		form = UserCreationForm()
+		form = SignupForm()
 	return render(request, 'accounts/signup_form.html', {
 		'form':form,
 	})
